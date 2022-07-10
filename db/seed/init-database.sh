9a22d874e597307fa0be534b19fae4f878161393
github akito-10 <akt.fukuda@gmail.com>
2022-07-10 18:28:00 +0900

    🐛 テーブルが作成できないバグ修正

--- db/seed/init-database.sh
+++ db/seed/init-database.sh
@@ -5,4 +5,4 @@
 #run the setup script to create the DB and the schema in the DB
 mysql -u go_test -pdocker go_database -p password < "/docker-entrypoint-initdb.d/0001_create_plan.sql"
 mysql -u go_test -pdocker go_database -p password < "/docker-entrypoint-initdb.d/0004_create_plan_participant_users.sql"
-mysql -u go_test -pdocker go_database -p password < "/docker-entrypoint-initdb.d/0005_create_plan_participant_users.sql"
+mysql -u go_test -pdocker go_database -p password < "/docker-entrypoint-initdb.d/0005_create_shop.sql"
