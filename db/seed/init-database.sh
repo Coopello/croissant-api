9c37472a0f0c37440f20577b4a995073070c284c
github akito-10 <akt.fukuda@gmail.com>
2022-07-10 18:10:43 +0900

    🎨 SHOPテーブル作成

--- db/seed/init-database.sh
+++ db/seed/init-database.sh
@@ -4,4 +4,5 @@
 
 #run the setup script to create the DB and the schema in the DB
 mysql -u go_test -pdocker go_database -p password < "/docker-entrypoint-initdb.d/0001_create_plan.sql"
-mysql -u go_test -pdocker go_database -p password < "/docker-entrypoint-initdb.d/0004_0004_create_plan_participant_users.sql"
+mysql -u go_test -pdocker go_database -p password < "/docker-entrypoint-initdb.d/0004_create_plan_participant_users.sql"
+mysql -u go_test -pdocker go_database -p password < "/docker-entrypoint-initdb.d/0005_create_plan_participant_users.sql"
